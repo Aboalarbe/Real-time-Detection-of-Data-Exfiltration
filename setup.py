e7bd22beb20645e08fe4538c6c0cd3b1ef8470ec
Mohamed Elaraby <mohamedaboalarbe@gmail.com>
2022-05-16 15:10:51 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A2 for ELG7186',
+    author='Mohamed Sayed',
+    license='',
+)
